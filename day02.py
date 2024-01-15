53808d63a0682d171c51934f365b705a5f3d6b8f
12201985 <12201985@inha.edu>
2024-01-15 15:44:16 +0900

    v1.2 python day02

--- day02.py
+++ day02.py
@@ -1,5 +1,9 @@
 # (100°F − 32) × 5/9 = 37.778°C
 # (0°C × 9/5) + 32 = 32°F
+# sum = 1+\
+#     2+\
+#     3       - \ 은 줄바꿈
+# print(sum)
 
 menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Quit program : ")
 
@@ -7,6 +11,7 @@ if menu == '1':
     fahrenheit = float(input('Input Fahrenheit : '))
     print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
 elif menu == '2':
-    celsius = float(input('Input Celcius : '))
+    celsius = float(input('Input Celsius : '))
     print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
-
+else:
+    print('Terminate Program.')

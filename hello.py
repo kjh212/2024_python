1880788850fe2db4271a67fd9273deec6f2f67b0
12201985 <12201985@inha.edu>
2024-01-16 17:49:17 +0900

    tree

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello Python~")  # alt+shift+F10
+i=k=0
+zero='*'
+dpf='III'
+while i<=10:
+    print('\033[32m'+zero.center(30)+'\033[0m')
+    i+=1
+    zero=zero+'*'+'*'
+while k<=2:
+    print('\033[31m'+dpf.center(30)+'\033[0m')
+    k+=1
+
+
+

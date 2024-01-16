98d63b85a9e275c16e5857cc3fe6b6b8e1a21716
12201985 <12201985@inha.edu>
2024-01-16 14:44:39 +0900

    v0.10 prime num

--- day03.py
+++ day03.py
@@ -1,12 +1,15 @@
 number=int(input("input number : "))
-is_prime = True
-i=2
-while i<number:
-    if number % i == 0:
-        is_prime = False
-        break
-    i+=1
-if is_prime :
-    print(f'{number} is prime number')
-else:
+is_prime = True #int->book
+if number <2:
     print(f'{number} is not prime')
+else:
+    i=2
+    while i<number:
+        if number % i == 0:
+            is_prime = False
+            break
+        i+=1
+    if is_prime :
+        print(f'{number} is prime number')
+    else:
+        print(f'{number} is not prime')

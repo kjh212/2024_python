b310e6cb8c1a18f62d73083ecb0e5d33b9952174
12201985 <12201985@inha.edu>
2024-01-16 13:37:46 +0900

    v0.6

--- day03.py
+++ day03.py
@@ -1,9 +1,30 @@
-setup= 'A duck goes into a bar...'
-print(setup.capitalize()) #처음 대문자
-print(setup.title()) #단어앞 전부 대문자
-print(setup.upper()) #전부 대문자
-print(setup.lower()) #전부 소문자
-print(setup.swapcase()) #대문자는 소문자 소문자는 대문자
-print(setup.center(30)) #중앙정렬
-print(setup.ljust(30)) #왼쪽정렬
-print(setup.rjust(30)) #오른쪽정렬
+# %s 문자열
+# %d 10진수
+# %x 16진수
+# %o 8진수
+# %f 10진 부동소수점
+# %e 지수로 나타낸 부동소수점
+print('%s' % 42)
+print('%d' % 42)
+print('%x' % 42)
+print('%o' % 42)
+
+print('%s' % 7.03)
+print('%f' % 7.03)
+print('%e' % 7.03)
+print('%g' % 7.03)
+
+print('%d%%' % 100) # %%=문자 %
+print("our cas %s weights %s pounds" % (40,50))
+
+thing='woodchuck'
+print('%s' % thing)
+print('%12s' % thing) # 12칸 오른쪽
+print('%+12s' % thing)
+print('%-12s' % thing) # 12칸 왼쪽 정렬
+print('%.3s' % thing) # 3개만 사용
+print('%12.3s' % thing) # 12칸 오른쪽 3개 사용
+print('%-12.3s' % thing)
+
+
+

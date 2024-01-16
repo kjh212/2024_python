cda8e781696ce5aa44ffb88e080a2742e56389cf
12201985 <12201985@inha.edu>
2024-01-16 13:20:35 +0900

    v0.5

--- day03.py
+++ day03.py
@@ -1,14 +1,9 @@
-subjects = "python c++ database linux"
-subject=input("수강신청과목 입력 : ")
-if subjects.find(subject) != -1: #index시 오류
-    print(f'해당과목 존재. 위치는 {subjects.find('c++')}번 인덱스')
-else:
-    print(' 해당과목 존재 x')
-
-subjects = "python c++ database linux"
-subject=input("수강신청과목 입력 : ")
-try:
-    print(f'해당과목존재. 위치는 {subjects.index(subject)}번 인덱스')
-except ValueError:
-    print('해당과목 존재 x')
-
+setup= 'A duck goes into a bar...'
+print(setup.capitalize())
+print(setup.title())
+print(setup.upper())
+print(setup.lower())
+print(setup.swapcase())
+print(setup.center(30))
+print(setup.ljust(30))
+print(setup.rjust(30))

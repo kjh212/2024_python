11be5d263afe9823631bbaececa8617b679d7d89
12201985 <12201985@inha.edu>
2024-01-16 18:57:27 +0900

    study

--- hello.py
+++ hello.py
@@ -1,13 +1,11 @@
-i=k=0
-zero='*'
-dpf='III'
-while i<=10:
-    print('\033[32m'+zero.center(30)+'\033[0m')
-    i+=1
-    zero=zero+'*'+'*'
-while k<=2:
-    print('\033[31m'+dpf.center(30)+'\033[0m')
-    k+=1
-
-
-
+y=[x for x in range (1,-10,-1)]
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ss=['123','234','2355']
+print('/'.join(ss))
+string='123123/23434/345345'
+print(string.split('/'))
+data=[2,4,3,1,5,10,9]
+data.sort()
+print(data)
+data.reverse()
+print(data)

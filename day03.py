410afa18c625bc13b676a8c6b81f3585a4d8595f
12201985 <12201985@inha.edu>
2024-01-16 13:04:19 +0900

    v0.4

--- day03.py
+++ day03.py
@@ -1,37 +1,14 @@
-# - split
-#course = "2024 KEB Bootcamp"
-#print(course)
-#list_course=course.split()
-#print(list_course)
-#list_course2=course.split('B')
-#print(list_course2)
+subjects = "python c++ database linux"
+subject=input("수강신청과목 입력 : ")
+if subjects.find(subject) != -1: #index시 오류
+    print(f'해당과목 존재. 위치는 {subjects.find('c++')}번 인덱스')
+else:
+    print(' 해당과목 존재 x')
 
-# - split 활용
-#numbers=input("Firstnumber SecondNumber : ").split('b')
-#print(int(numbers[0]) + int(numbers[1]))
+subjects = "python c++ database linux"
+subject=input("수강신청과목 입력 : ")
+try:
+    print(f'해당과목존재. 위치는 {subjects.index(subject)}번 인덱스')
+except ValueError:
+    print('해당과목 존재 x')
 
-# -join
-subjects=["python","c++","database"]
-subjects_string='/'.join(subjects)
-print(subjects_string)
-
-# - replace
-course = "* KEB 2024 KEB Bootcamp KEB.!#"
-print(course.replace("KEB","inha"))
-print(course)
-course = course.replace("KEB","inha")
-print(course)
-print(course.replace("2","1",2))
-
-# - strip
-print(course.strip())
-print(course.strip("!.#*"))
-
-# -find,index
-course = "* KEB 2024 KEB Bootcamp KEB.!#"
-print(course.find('KEB'))
-print(course.rfind('KEB'))
-print(course.index("KEB"))
-
-print(course.find('inha')) #-1
-print(course.index('inha')) #ValueError

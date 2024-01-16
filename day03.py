1771d09d6eed7907d9fd377dd1a161b529f63424
12201985 <12201985@inha.edu>
2024-01-16 13:22:37 +0900

    v0.5

--- day03.py
+++ day03.py
@@ -1,9 +1,9 @@
 setup= 'A duck goes into a bar...'
-print(setup.capitalize())
-print(setup.title())
-print(setup.upper())
-print(setup.lower())
-print(setup.swapcase())
-print(setup.center(30))
-print(setup.ljust(30))
-print(setup.rjust(30))
+print(setup.capitalize()) #처음 대문자
+print(setup.title()) #단어앞 전부 대문자
+print(setup.upper()) #전부 대문자
+print(setup.lower()) #전부 소문자
+print(setup.swapcase()) #대문자는 소문자 소문자는 대문자
+print(setup.center(30)) #중앙정렬
+print(setup.ljust(30)) #왼쪽정렬
+print(setup.rjust(30)) #오른쪽정렬

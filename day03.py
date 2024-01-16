56874ab2090ebda68c779cca729346482bba2a9c
12201985 <12201985@inha.edu>
2024-01-16 13:49:06 +0900

    v0.7

--- day03.py
+++ day03.py
@@ -1,30 +1,15 @@
-# %s 문자열
-# %d 10진수
-# %x 16진수
-# %o 8진수
-# %f 10진 부동소수점
-# %e 지수로 나타낸 부동소수점
-print('%s' % 42)
-print('%d' % 42)
-print('%x' % 42)
-print('%o' % 42)
+subjects={'python' : 'kim', 'c++':'sung', 'database':'kang','data structure':'kim'}
+print("{0[c++]} {0[data structure]}".format(subjects))
 
-print('%s' % 7.03)
-print('%f' % 7.03)
-print('%e' % 7.03)
-print('%g' % 7.03)
-
-print('%d%%' % 100) # %%=문자 %
-print("our cas %s weights %s pounds" % (40,50))
-
-thing='woodchuck'
-print('%s' % thing)
-print('%12s' % thing) # 12칸 오른쪽
-print('%+12s' % thing)
-print('%-12s' % thing) # 12칸 왼쪽 정렬
-print('%.3s' % thing) # 3개만 사용
-print('%12.3s' % thing) # 12칸 오른쪽 3개 사용
-print('%-12.3s' % thing)
-
-
-
+thing='wraith'
+place='window'
+print(('the {} is at the {}').format(thing,place))
+    #the wraith is at the window
+print(('the {:10s} is at the {:10s}').format(thing,place))
+    #the wraith     is at the window
+print(('the {:<10s} is at the {:<10s}').format(thing,place))
+    #the wraith     is at the window
+print(('the {:^10s} is at the {:^10s}').format(thing,place))
+    #the   wraith   is at the   window
+print(('the {:!^10s} is at the {:#^10s}').format(thing,place))
+    #the !!wraith!! is at the ##window##

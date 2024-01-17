57378de3f0dbfdd37b772318504f3253bdab6707
12201985 <12201985@inha.edu>
2024-01-17 18:57:08 +0900

    hw 8.6,p205,day03

--- pra.py
+++ pra.py
@@ -1,28 +1,25 @@
-#6.1
-y=[x for x in range (3,-1,-1)]
-print(y)
-
-#6.2
-guess_me=7
-number=1
-while number<=guess_me+1:
-    if number<guess_me:
-        print(f'{number} too low')
-    elif number==guess_me:
-        print('found it!')
-    elif number>guess_me:
-        print('oops')
-        break
-    number+=1
-
-#6.3
-guess_me=5
-for number in range (10):
-    if number<guess_me:
-        print(f'{number} too low')
-    elif number==guess_me:
-        print('found it!')
-    elif number>guess_me:
-        print('oops')
-
-
+#8.1
+e2f={'dog':'chien','cat':'chat','walrus':'morse'}
+print(e2f)
+#8.2
+print(e2f['walrus'])
+#8.3
+f2e={}
+for e,f in e2f.items():
+    f2e[f]=e
+print(f2e)
+#8.4
+print(f2e['chien'])
+#8.5
+print(set(e2f.keys()))
+#8.6
+life={'animal':{'cat':['Henri','Grumpy','Lucy'],'octopi':{},'emus':{}},'plants':{},'other':{}}
+#8.7
+print(set(life.keys()))
+#8.8
+print(set(life['animal'].keys()))
+#8.9
+print(set(life['animal']['cat']))
+#8.10
+squares={x: x*x for x in range(10)}
+print(squares)

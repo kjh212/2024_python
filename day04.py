94bb3909aaf2e15ff3510b7ac48c6321afa4c2ac
12201985 <12201985@inha.edu>
2024-01-17 15:49:12 +0900

    v0.6 day03 (ãupdate)

--- day04.py
+++ day04.py
@@ -10,12 +10,13 @@
 import random
 
 drinks_foods = {"위스키":"초콜릿","와인":"치즈","소주":"삼겹살","고량주":"양꼬치"}
-
+japan_drinks={"사케":"광어회","위스키":"낙곱새"}
+drinks_foods.update(japan_drinks)
 #drink = input(drinks_foods.keys())
 drinks_food_keys=list(drinks_foods)
 print(drinks_food_keys)
 while True:
-    menu = input(f'다음 술중에 고르세요.\n1){drinks_food_keys[0]} 2){drinks_food_keys[1]} 3){drinks_food_keys[2]} 4){drinks_food_keys[3]} 5)랜덤, 6)종료 : ')
+    menu = input(f'다음 술중에 고르세요.\n1){drinks_food_keys[0]} 2){drinks_food_keys[1]} 3){drinks_food_keys[2]} 4){drinks_food_keys[3]} 5){drinks_food_keys[4]} 6)랜덤 7)종료 : ')
     if menu =='1':
         print(f'{drinks_food_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_food_keys[0]]}입니다.')
     elif menu =='2':
@@ -24,9 +25,11 @@ while True:
         print(f'{drinks_food_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_food_keys[2]]}입니다.')
     elif menu =='4':
         print(f'{drinks_food_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_food_keys[3]]}입니다.')
-    elif menu =='5':
+    elif menu == '5':
+        print(f'{drinks_food_keys[4]}에 어울리는 안주는 {drinks_foods[drinks_food_keys[4]]}입니다.')
+    elif menu =='6':
         random_drink = random.choice(drinks_food_keys)
         print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]}입니다.')
-    elif menu =='6':
+    elif menu =='7':
         print(f'다음에 또 오세요')
         break

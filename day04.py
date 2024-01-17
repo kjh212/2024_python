78785cfd21bc2e263f38dbc80825dae74b70088f
12201985 <12201985@inha.edu>
2024-01-17 14:38:20 +0900

    v0.4 day03 (zip)

--- day04.py
+++ day04.py
@@ -1,18 +1,10 @@
-import copy
+x='1','2','3'
+y='4','5','6'
+z=list(zip(x,y))
+r=dict(zip(x,y))
+print(z,r)
+#z=[('1', '4'), ('2', '5'), ('3', '6')]
+#r={'1': '4', '2': '5', '3': '6'}
 
-subjects = ["a",["b","c"],"d"]
-a=subjects
-b=subjects.copy()
-c=list(subjects)
-d=subjects[:]
-e=copy.deepcopy(a)
-print(subjects,a,b,c,d,e)
-subjects[0]='x'
-subjects[1][1]='y'
-print(subjects,a,b,c,d,e)
-#subjects=['x', ['b', 'y'], 'd']
-#a=['x', ['b', 'y'], 'd']
-#b=['a', ['b', 'y'], 'd']
-#c=['a', ['b', 'y'], 'd']
-#d=['a', ['b', 'y'], 'd']
-#e=['a', ['b', 'c'], 'd']
+number_list=[x*x for x in range(1,6) if x%2==1]
+print(number_list)

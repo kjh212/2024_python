814e9211cbae014d6309699a107fedbe8c9ab052
12201985 <12201985@inha.edu>
2024-01-19 13:46:27 +0900

    v0.8 day06

--- day06.py
+++ day06.py
@@ -8,3 +8,16 @@ pickchu.nemesis=squirtle
 print(pickchu.name)
 squirtle.name="Gobok"
 print(pickchu.nemesis.name)
+
+
+class Pokemon:
+    def __init__(self,name):
+        self.name=name
+        print(f"{name} 포켓몬스터 생성")
+
+    def attack(self,target):
+        print(f"{self.name}이 {target.name}을 공격")
+
+ch=Pokemon('리자몽')
+pickchu=Pokemon('피카츄')
+squirtle=Pokemon('꼬북이')

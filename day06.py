c7f76134b79360f945c57eb34e54b542063aba2e
12201985 <12201985@inha.edu>
2024-01-19 15:08:15 +0900

    v0.8(a) day06

--- day06.py
+++ day06.py
@@ -1,23 +1,30 @@
+# class Pokemon:
+#     pass
+#
+# pikachu = Pokemon()
+# squirtle = Pokemon()
+# pikachu.name = "피카츄"
+# pikachu.nemesis = squirtle
+# print(pikachu.name)
+# #print(pikachu.nemesis.name)
+# pikachu.nemesis.name = "꼬부기"  #squirtle.name = "꼬부기"
+# print(pikachu.nemesis.name)
+# print(squirtle.name)
+
+
+#class Pokemon():
 class Pokemon:
-    pass
-
-pickchu=Pokemon()
-squirtle=Pokemon()
-pickchu.name="pizk"
-pickchu.nemesis=squirtle
-print(pickchu.name)
-squirtle.name="Gobok"
-print(pickchu.nemesis.name)
-
-
-class Pokemon:
-    def __init__(self,name):
-        self.name=name
+    def __init__(self, name):
+        self.name = name
         print(f"{name} 포켓몬스터 생성")
 
-    def attack(self,target):
-        print(f"{self.name}이 {target.name}을 공격")
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을(를) 공격!")
 
-ch=Pokemon('리자몽')
-pickchu=Pokemon('피카츄')
-squirtle=Pokemon('꼬북이')
+
+charizard = Pokemon("리자몽")
+pikachu = Pokemon("피카츄")
+squirtle = Pokemon("꼬부기")
+charizard.attack(squirtle)
+# print(pikachu.name)
+# print(squirtle.name)

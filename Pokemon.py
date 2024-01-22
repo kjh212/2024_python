b235af01517f4130f5fbaa96dbde7b0ca8f791a9
12201985 <12201985@inha.edu>
2024-01-22 11:28:55 +0900

    v0.5 pokemon game

--- Pokemon.py
+++ Pokemon.py
@@ -12,34 +12,39 @@ class Pikachu(Pokemon):
 
 
     def attack(self,enemy):
-        print(f'{enemy.name}의 HP는 {enemy.HP}이다.')
+        print(f'{enemy.name}의 HP는 {enemy.HP}이다.(enter)')
+        input('')
+        print('다음 중 선택하세요.(enter)')
+        input('')
         while enemy.HP > 0:
             attackinput = int(input('1.몸통박치기\n2.백만볼트\n3.도망치기\n-->'))
             if attackinput==1:
                 enemy.HP=enemy.HP-2
-                print('효과는 괜찮았다.')
+                print('효과는 괜찮았다.(enter)')
                 input('')
             elif attackinput==2:
                 if enemy.attribute=='물속성':
                     enemy.HP=enemy.HP-3
-                    print('효과는 굉장했다.')
+                    print('효과는 굉장했다.(enter)')
                 elif enemy.attribute=='전기속성':
                     enemy.HP=enemy.HP-2
-                    print('효과는 괜찮았다.')
+                    print('효과는 괜찮았다.(enter)')
                 elif enemy.attribute=='불속성':
                     enemy.HP=enemy.HP-1
-                    print('효과는 미미했다.')
+                    print('효과는 미미했다.(enter)')
                 input('')
             elif attackinput==3:
+                print('상대가 너무 강하다..도망을 선택했다.(enter)')
+                input('')
                 break
             else:
-                print('다시 선택하십쇼.')
+                print('다시 선택하십쇼.(enter)')
                 input('')
 
             if enemy.HP > 0:
                 print(f'남은 체력은 {enemy.HP}이다. ')
             elif enemy.HP <= 0:
-                print(f'{enemy.name}을 물리쳤다!!!')
+                print(f'{enemy.name}을 물리쳤다!!!(enter)')
                 input('')
         your_poke = Pikachu()
         startpokemon.activity(your_poke)
@@ -53,34 +58,39 @@ class Charmander(Pokemon):
         super().__init__('파이리','불속성',9)
 
     def attack(self, enemy):
-        print(f'{enemy.name}의 HP는 {enemy.HP}이다.')
+        print(f'{enemy.name}의 HP는 {enemy.HP}이다.(enter)')
+        input('')
+        print('다음 중 선택하세요.(enter)')
+        input('')
         while enemy.HP > 0:
             attackinput = int(input('1.머리박치기\n2.불대포\n3.도망치기\n-->'))
             if attackinput == 1:
                 enemy.HP = enemy.HP - 2
-                print('효과는 괜찮았다.')
+                print('효과는 괜찮았다.(enter)')
                 input('')
             elif attackinput == 2:
                 if enemy.attribute == '전기속성':
                     enemy.HP = enemy.HP - 3
-                    print('효과는 굉장했다.')
+                    print('효과는 굉장했다.(enter)')
                 elif enemy.attribute == '불속성':
                     enemy.HP = enemy.HP - 2
-                    print('효과는 괜찮았다.')
+                    print('효과는 괜찮았다.(enter)')
                 elif enemy.attribute == '물속성':
                     enemy.HP = enemy.HP - 1
-                    print('효과는 미미했다.')
+                    print('효과는 미미했다.(enter)')
                 input('')
             elif attackinput == 3:
+                print('상대가 너무 강하다..도망을 선택했다. (enter)')
+                input('')
                 break
             else:
-                print('다시 선택하십쇼.')
+                print('다시 선택하십쇼. (enter)')
                 input('')
 
             if enemy.HP > 0:
-                print(f'남은 체력은 {enemy.HP}이다. ')
+                print(f'남은 체력은 {enemy.HP}이다.')
             elif enemy.HP <= 0:
-                print(f'{enemy.name}을(를) 물리쳤다!!!')
+                print(f'{enemy.name}을(를) 물리쳤다!!!(enter)')
                 input('')
         your_poke = Charmander()
         startpokemon.activity(your_poke)
@@ -89,34 +99,39 @@ class Squirtle(Pokemon):
         super().__init__('꼬부기','물속성',11)
 
     def attack(self, enemy):
-        print(f'{enemy.name}의 HP는 {enemy.HP}이다.')
+        print(f'{enemy.name}의 HP는 {enemy.HP}이다.(enter)')
+        input('')
+        print('다음 중 선택하세요.(enter)')
+        input('')
         while enemy.HP > 0:
             attackinput = int(input('1.머리박치기\n2.물대포\n3.도망치기\n-->'))
             if attackinput == 1:
                 enemy.HP = enemy.HP - 2
-                print('효과는 괜찮았다.')
+                print('효과는 괜찮았다. (enter)')
                 input('')
             elif attackinput == 2:
                 if enemy.attribute == '불속성':
                     enemy.HP = enemy.HP - 3
-                    print('효과는 굉장했다.')
+                    print('효과는 굉장했다.(enter)')
                 elif enemy.attribute == '물속성':
                     enemy.HP = enemy.HP - 2
-                    print('효과는 괜찮았다.')
+                    print('효과는 괜찮았다.(enter)')
                 elif enemy.attribute == '전기속성':
                     enemy.HP = enemy.HP - 1
-                    print('효과는 미미했다.')
+                    print('효과는 미미했다.(enter)')
                 input('')
             elif attackinput == 3:
+                print('상대가 너무 강하다..도망을 선택했다.(enter)')
+                input('')
                 break
             else:
-                print('다시 선택하십쇼.')
+                print('다시 선택하십쇼.(enter)')
                 input('')
 
             if enemy.HP > 0:
                 print(f'남은 체력은 {enemy.HP}이다. ')
             elif enemy.HP <= 0:
-                print(f'{enemy.name}을 물리쳤다!!!')
+                print(f'{enemy.name}을 물리쳤다!!!(enter)')
                 input('')
         your_poke=Squirtle()
         startpokemon.activity(your_poke)
@@ -138,7 +153,7 @@ class pokemon:
     def start(self):
         print('게임을 시작하려면 enter을 누르시오.')
         input('')
-        print('포켓몬 월드에 오신 것을 환영합니다.')
+        print('포켓몬 월드에 오신 것을 환영합니다.(enter)')
         input('')
         print('당신의 포켓몬을 고르시오.')
         startpokemon.choice()
@@ -147,13 +162,13 @@ class pokemon:
         while True:
             num = int(input('1:피카츄\n2:파이리\n3:꼬부기\n-->'))
             if num!=1 and num!=2 and num!=3:
-                print('다시 선택하십쇼.')
+                print('다시 선택하십쇼.(enter)')
                 input('')
                 continue
             print(f'{pokemon_who[num - 1]}을 선택하시겠습니까?')
             answer1 = input("1.예\n2.아니오\n-->")
             if answer1 == "1":
-                print(f'{pokemon_who[num - 1]}를 선택하셨습니다.')
+                print(f'{pokemon_who[num - 1]}를 선택하셨습니다.(enter)')
                 if num==1:
                     your_pokemon=Pikachu()
                 elif num==2:
@@ -164,8 +179,10 @@ class pokemon:
                 print(f'{pokemon_who[num - 1]}와 함께 무엇을 하실 것인가요?')
                 startpokemon.activity(your_pokemon)
                 break
-            elif answer1 == "2":
+            else:
                 print('다시 선택하십쇼.')
+                input('')
+                continue
 
     def activity(self,your_pokemon):
         while True:
@@ -174,24 +191,38 @@ class pokemon:
             enemy = monster_choice()
             active1 = int(input('1:풀숲으로 들어간다.\n2.게임 종료.\n-->'))
             if active1 == 1:
-                print(f'야생의 {enemy.name}이(가) 나타났다.')
+                print(f'야생의 {enemy.name}이(가) 나타났다.(enter)')
                 input('')
-                b=int(input('1.싸운다.\n2.도망간다.\n-->'))
-                if b==1:
-                    your_pokemon.attack(enemy)
-                    break
-                else:
-                    pass
+                while True:
+                    b=int(input('1.싸운다.\n2.도망간다.\n-->'))
+                    if b==1:
+                        your_pokemon.attack(enemy)
+                        break
+                    elif b==2:
+                        print('상대가 너무 강하다..도망을 선택했다.(enter)')
+                        input('')
+                        break
+                    else:
+                        print('다시 입력하세요(enter)')
+                        input('')
+                        continue
 
             elif active1 == 2:
                 print('게임을 종료하시겠습니까?')
                 a=int(input('1.예\n2.아니오.\n-->'))
                 if a == 1:
-                    break
+                    sys.exit()
                 if a == 2:
-                    print('다시 선택하세요')
+                    continue
+                else :
+                    print('다시 입력하세요(enter)')
                     input('')
                     pass
+            else:
+                print('다시 선택하십쇼(enter)'
+                      '')
+                input('')
+                continue
 
 
 

6d80cf944bdccca45fba43eeecf4f03d226b694c
12201985 <12201985@inha.edu>
2024-01-22 14:23:28 +0900

    day07

---
+++ day07.py
@@ -0,0 +1,23 @@
+class FlyingBehavior:
+    def fly(self):
+        return f"하늘을 훨훨 날아갑니다~"
+class JetPack(FlyingBehavior):
+    def fly(self):
+        return f"로켓추진기로 하늘을 날아갑니다!"
+class NoFly(FlyingBehavior):
+    def fly(self):
+        return f"하늘을 날 수 없습니다."
+class FlyWithWings(FlyingBehavior):
+    def fly(self):
+        return f"날개로 하늘을 훨훨 날아갑니다"
+
+
+class Pikachu:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        self.fly_behavior = NoFly()  # composition
+
+
+p1 = Pikachu("피카츄", 35)
+print(p1.fly_behavior.fly())
